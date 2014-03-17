1f5f853ad5aa42b1bb4d0769fe10fa76f429ed41
Mike Nolta <mike@nolta.net>
2014-03-17 19:32:04 -0400

    add wcsmodify

--- src/WCSLIB.jl
+++ src/WCSLIB.jl
@@ -1,6 +1,8 @@
 module WCSLIB
 
-export wcsalloc
+export pvcard,
+       wcsalloc,
+       wcsmodify
 
 using BinDeps
 @BinDeps.load_dependencies
@@ -8,15 +10,56 @@ using BinDeps
 include("libwcs_common.jl")
 include("libwcs_h.jl")
 
-const wcsprm_offset = Dict(names(wcsprm), fieldoffsets(wcsprm))
+for (name,offset) in zip(names(wcsprm),fieldoffsets(wcsprm))
+    if name in (:flag,:npv)
+        @eval const $(symbol(string("_",name,"_offset"))) = $offset
+    end
+end
 
 function wcsalloc(naxis::Integer)
     p = convert(Ptr{wcsprm}, c_malloc(sizeof(wcsprm)))
-    pflag = p + wcsprm_offset[:flag]
-    unsafe_store!(convert(Ptr{Cint}, pflag), -1)
+    unsafe_store!(convert(Ptr{Cint}, p+_flag_offset), -1)
     stat = wcsini(1, naxis, p)
     @assert stat == 0
     p
+end
+
+function unsafe_store_vec!{T}(p::Ptr{T}, v::Vector{T})
+    for i = 1:length(v)
+        unsafe_store!(p, v[i], i)
+    end
+end
+
+function wcsmodify(p::Ptr{wcsprm}; kvs...)
+    w = unsafe_load(p)
+    naxis = w.naxis
+    for (k,v) in kvs
+        if k in (:cdelt,:crder,:crpix,:crval,:csyer)
+            @assert isa(v, Vector{Float64})
+            @assert length(v) == naxis
+            unsafe_store_vec!(w.(k), v)
+
+        elseif k in (:cname,:ctype,:cunit)
+            @assert isa(v, Vector{ASCIIString})
+            @assert length(v) == naxis
+            pf = convert(Ptr{Uint8}, w.(k))
+            n = 72
+            for i = 1:naxis
+                @assert length(v[i]) < n
+                unsafe_store_vec!(pf + n*(i-1), v[i].data)
+            end
+
+        elseif k === :pv
+            npv = length(v)
+            @assert isa(v, Vector{pvcard})
+            @assert npv <= w.npvmax
+            unsafe_store_vec!(w.pv, v)
+            unsafe_store!(convert(Ptr{Cint}, p+_npv_offset), npv)
+
+        else
+            error("unrecognized keyword argument \"$k\"")
+        end
+    end
 end
 
 end # module
