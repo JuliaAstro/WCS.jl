95531ddb756f5caf648df6ddda4632a3a5f67c2b
Mike Nolta <mike@nolta.net>
2014-05-01 13:42:12 -0400

    bump upstream version (4.21 -> 4.22)

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,7 @@
 using BinDeps
 @BinDeps.setup
 
-version = "4.21"
+version = "4.22"
 url = "ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-$version.tar.bz2"
 
 wcs = library_dependency("libwcs")
