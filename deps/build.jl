259b8847f6362ac73307aad4082b8e4036fa5851
Mike Nolta <mike@nolta.net>
2014-08-27 00:20:37 -0400

    fix parallel build error (closes #1)

--- deps/build.jl
+++ deps/build.jl
@@ -6,7 +6,23 @@ url = "ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-$version.tar.bz2"
 
 wcs = library_dependency("libwcs")
 provides(Sources, URI(url), wcs, unpacked_dir="wcslib-$version")
-builddir = joinpath(BinDeps.depsdir(wcs), "src/wcslib-$version")
-provides(BuildProcess, Autotools(libtarget="C/libwcs.so.$version", builddir=builddir), wcs)
+depsdir = BinDeps.depsdir(wcs)
+builddir = joinpath(depsdir, "src/wcslib-$version")
+prefix = joinpath(depsdir, "usr")
+@unix_only libfilename = "libwcs.so.$version"
+@osx_only libfilename = "libwcs.dylib"
+provides(BuildProcess,
+         (@build_steps begin
+            GetSources(wcs)
+            @build_steps begin
+              ChangeDirectory(builddir)
+              FileRule(joinpath(prefix,"lib",libfilename),
+                       @build_steps begin
+                         `./configure --prefix=$prefix`
+                         `make install`
+                       end)
+            end
+          end),
+         wcs)
 
 @BinDeps.install [:libwcs => :libwcs]
