326cb5d6ed668f30cadfb1e60fac83e22411c4d4
Miles Lucas <mdlucas@hawaii.edu>
2022-01-06 13:37:19 -0600

    update docs make.jl and make sure Manifests are checked in

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,24 @@
-using Documenter
 using WCS
+using Documenter
 
-makedocs(
-    sitename = "WCS.jl",
-    format = Documenter.HTML(),
-    modules = [WCS],
-    strict = true,
+DocMeta.setdocmeta!(WCS, :DocTestSetup, :(using WCS); recursive=true)
+
+makedocs(;
+    modules=[WCS],
+    authors="JuliaAstro contributors",
+    repo="https://github.com/JuliaAstro/WCS.jl/blob/{commit}{path}#{line}",
+    sitename="WCS.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://JuliaAstro.github.io/WCS.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
 )
 
-deploydocs(
-    repo = "github.com/JuliaAstro/WCS.jl.git"
+deploydocs(;
+    repo="github.com/JuliaAstro/WCS.jl",
+    devbranch="master",
 )
