05486d26106ecd241c891bccae6398e36da73ce7
Ian Weaver <weaveric@gmail.com>
2025-12-18 16:49:15 -0800

    comment out until upstream resolved

--- test/test_docs.jl
+++ test/test_docs.jl
@@ -1,6 +1,6 @@
-using Documenter: DocMeta, doctest
-
-@testset "Doctests" begin
-    DocMeta.setdocmeta!(WCS, :DocTestSetup, :(using WCS); recursive=true)
-    doctest(WCS)
-end
+#using Documenter: DocMeta, doctest
+#
+#@testset "Doctests" begin
+#    DocMeta.setdocmeta!(WCS, :DocTestSetup, :(using WCS); recursive=true)
+#    doctest(WCS)
+#end
